a1cc696e8b1c392f8b8cd8cfcecb9e2d365ede54
Dorain <Dorain@MacBook-Pro-48.attlocal.net>
2017-02-23 19:56:32 -0800

    binary search

--- 441. Arranging Coins.java
+++
@@ -1,9 +0,0 @@
-public class Solution {
-    public int arrangeCoins(int n) {
-        int k = (int)Math.sqrt((double)n*2) - 1;
-        while(k*(k+1)<=2*n){
-            k++;
-        }
-        return k-1;
-    }
-}
