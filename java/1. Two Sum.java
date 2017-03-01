dffc91ee037a1fc915048f175052055aa05c2be4
Dorain <patrickgu001@gmail.com>
2017-02-28 20:46:32 -0800

    array

---
+++ java/1. Two Sum.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = {-1,-1};
+        if(nums == null || nums.length == 0) return result;
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for(int i = 0; i< nums.length; i++){
+            if(map.containsKey(nums[i])){
+              result[0] = map.get(nums[i]);
+              result[1] = i;
+              return result;
+            } 
+            map.put(target-nums[i], i);
+        }
+        return result;
+    }
+}
