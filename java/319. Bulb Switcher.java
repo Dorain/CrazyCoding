73aab25fc6f11ed2076422ef1591e6fe179bd0b3
Dorain <patrickgu001@gmail.com>
2017-04-20 19:03:34 -0700

    math

---
+++ java/319. Bulb Switcher.java
@@ -0,0 +1,6 @@
+public class Solution {
+    public int bulbSwitch(int n) {
+        // amazing, only squre number has odd factors, which means they are on 
+        return (int)Math.sqrt(n);
+    }
+}
