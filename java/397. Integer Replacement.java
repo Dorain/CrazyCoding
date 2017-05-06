5ae05d00f18bf668d547322c1911600e938db14e
Dorain <patrickgu001@gmail.com>
2017-05-06 15:39:58 -0700

    math

---
+++ java/397. Integer Replacement.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public int integerReplacement(int n) {
+        int count = 0;
+        while(n > 1){
+            if(n%2 == 0) n= n/2;
+            else {
+                if(n == Integer.MAX_VALUE) return 32; // control overflow
+                n = ((n+1)%4 == 0 && (n-1) != 2) ? n+1 : n-1;
+            }
+            count ++;
+        }
+        return count;
+    }
+}
