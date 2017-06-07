2907e967954c6db2456ac1879da8e3fadba50321
Dorain <patrickgu001@gmail.com>
2017-06-07 11:55:39 -0700

    dp

---
+++ java/70. Climbing Stairs.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public int climbStairs(int n) {
+        int[] step = new int[n+1];
+	step[0] = step[1] = 1;
+	for(int i = 2; i <= n; i++){
+		step[i] = step[i-2] + step[i-1];
+	}
+	return step[n];
+    }
+}
